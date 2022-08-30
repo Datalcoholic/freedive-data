7d777eebc0c9574de2a8d697481d460b69f401c4
R.E <you@example.com>
2022-08-30 12:29:04 -0400

    feat: :sparkles: read json file

--- myApp.js
+++ myApp.js
@@ -1,1 +1,6 @@
-console.log('test');
+import { readFileSync } from 'fs';
+
+let rawData = readFileSync('response_1661024483443.json');
+let rawRecods = JSON.parse(rawData);
+
+rawRecods[0].forEach((e) => console.log(e));
