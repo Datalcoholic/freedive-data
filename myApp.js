cdff8f8fad2428754d2db46cbc98ff5b4b338446
R.E <you@example.com>
2022-08-30 16:53:05 -0400

    refactor: :construction: cleaninf and wranwrking data

--- myApp.js
+++ myApp.js
@@ -3,4 +3,21 @@ import { readFileSync } from 'fs';
 let rawData = readFileSync('response_1661024483443.json');
 let rawRecods = JSON.parse(rawData);
 
-rawRecods[0].forEach((e) => console.log(e));
+let cleanRecords = rawRecods[0].map((e) => {
+	const regex = /\(([^()]*)\)/;
+	const { Date, Depth_m, Discipline, Distance_m, Gender, Name, Place, Time } =
+		e;
+	return {
+		date: Date,
+		depth_m: +Depth_m,
+		discipline: Discipline,
+		disciplineShort: Discipline.match(regex)[0],
+		distance_m: +Distance_m,
+		gender: Gender,
+		name: Name,
+		place: Place,
+		time: +Time,
+	};
+});
+
+console.log(cleanRecords);
